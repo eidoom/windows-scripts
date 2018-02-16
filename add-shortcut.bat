2fe1bff480e3aeca552875ba8fa160eeeb4ade49
ryan <ryanmoodie@gmail.com>
2018-02-16 12:51:15 +0000

    Added note scraper

--- add-shortcut.bat
+++ add-shortcut.bat
@@ -1,4 +1,5 @@
 @ECHO off
 cd C:\Users\ryan\scripts
+echo Do not use file suffixes
 powershell C:\Users\ryan\scripts\create-shortcut.ps1
 bash /home/ryan/scripts/add-shortcut
---
+++ download-notes.bat
@@ -0,0 +1,7 @@
+@ECHO off
+
+SET PATH=C:\Users\ryan\Anaconda3
+
+cd C:\sync\projects\scrape-aqft-notes
+
+python download.py
