674f79da3f0c03c313685045487961bb3812d86c
ryan <ryanmoodie@gmail.com>
2018-04-21 19:22:12 +0100

    Updated export-term-lectures for new term

--- export-term-lectures.bat
+++ export-term-lectures.bat
@@ -5,7 +5,7 @@ cd C:\Programs\OneNote2PDF_0.3
 set temp="C:\Programs\OneNote2PDF_0.3\temp"
 set directory=C:\sync\physics\6_Part_iii\Courses
 
-set courses= AQFT BH SM ST
+set courses= CQS SUSY
 
 FOR %%i in (%courses%) do (
     OneNote2PDF -Notebook "%%i" -CacheFolder %temp% -Output "%directory%\%%i\Exports" -Exclude OneNote_RecycleBin -ExportSection "Lectures_1"
