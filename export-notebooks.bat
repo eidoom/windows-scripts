bd1e6565d481b1485fe244355e8ab6ba61e20e47
ryan <ryanmoodie@gmail.com>
2018-10-16 13:37:35 +0100

    Update

--- export-notebooks.bat
+++ export-notebooks.bat
@@ -12,7 +12,7 @@ echo.
 
 set notesbooksReading=calculating-scattering-amplitudes-efficiently
 set notebooksTeaching=foundations-of-physics-3a-2018
-set notebooksLectures=qft
+set notebooksLectures=ampl astro cft cmp cos flav gr grp higgs ift npp nu qcd qed qft rg sm strings susy
 
 for %%i in (%notesbooksReading%) do (
     echo # Exporting notebook %%i to path %directory%\Teaching\%%i
