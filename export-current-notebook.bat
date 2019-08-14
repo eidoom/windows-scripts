5e3d19540bc5a2c9e9af612064f4a1c10388d5ec
ryan <ryanmoodie@gmail.com>
2019-08-14 17:15:18 +0100

    Update

--- export-current-notebook.bat
+++ export-current-notebook.bat
@@ -13,3 +13,12 @@ for %%i in (%notesbooksCurrent%) do (
     C:\Programs\OneNote2PDF_0.3\OneNote2PDF.exe -Notebook "%%i" -CacheFolder "%temp%" -Output "%directory%" -Exclude OneNote_RecycleBin -ExportNotebook true -ShowTOC true
     echo.
 )
+
+set notesbooksCurrent2=BUSSTEPP
+set directory2=C:\Users\ryan\gdrive-durham\PhD\Conferences\BUSSTEPP-2019
+
+for %%i in (%notesbooksCurrent2%) do (
+    echo # Exporting notebook %%i to path %directory%
+    C:\Programs\OneNote2PDF_0.3\OneNote2PDF.exe -Notebook "%%i" -CacheFolder "%temp%" -Output "%directory2%" -Exclude OneNote_RecycleBin -ExportNotebook true -ShowTOC true
+    echo.
+)
